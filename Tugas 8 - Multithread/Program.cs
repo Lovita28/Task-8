fd5b70a2012cac1a37f98a253be4fdf43c5f0e00
Lovita28 <lovitaanjani@gmail.com>
2017-07-26 12:08:18 +0700

    Tugas C# Multithread

---
+++ Tugas 8 - Multithread/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Tugas_8___Multithread
+{
+    class Program
+    {
+        public void DoTest()
+        {
+            Thread t1 =
+                new Thread(
+                    new ThreadStart(Incrementer));
+            Thread t2 =
+                new Thread(
+                    new ThreadStart(Decrementer));
+            t1.Start();
+            t2.Start();
+        }
+        public void Incrementer()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                System.Console.WriteLine("Penambah : {0} ", i);
+                Thread.Sleep(1000);
+            }
+        }
+        public void Decrementer()
+        {
+            for (int i = 10; i >= 0; i--)
+            {
+                if (i == 5)
+                {
+                    Console.WriteLine("Decrement End");
+                    i = 0;
+                }
+                else
+                {
+                    System.Console.WriteLine("Pengurangan : {0} ", i);
+                    Thread.Sleep(1000);
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            Program t = new Program();
+            Console.WriteLine("Start");
+            t.DoTest();
+            Console.ReadKey();
+        }
+    }
+}
